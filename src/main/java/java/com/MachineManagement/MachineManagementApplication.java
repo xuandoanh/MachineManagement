c19d9a61254bc36709ea09869262b9701337a950
xuandoanh122 <xuandoanh122@gmail.com>
2025-03-14 11:52:14 +0700

    Initial commit

---
+++ src/main/java/java/com/MachineManagement/MachineManagementApplication.java
@@ -0,0 +1,13 @@
+package java.com.MachineManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachineManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/com/MachineManagement/MachineManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.MachineManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
